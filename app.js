813faf85f83a0ad3e078e00c684d901f44eae8bb
Kyaw-Swar-Linn <kyawswarlinn@gicjp.biz>
2021-10-12 16:23:39 +0630

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello GIC Myanmar!'));
+app.get('/', (req, res) => res.send('Hello GIC Myanmar And GIC Japan !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+ 
