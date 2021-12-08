92078aa52bf292142d3d08106f4c2b1ea6a40a16
Azu_Mon@gicjp.biz <kyawswarlinn@gicjp.biz>
2021-12-08 18:28:07 +0630

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Hello World!'));
+app.get('/', (req, res) => res.send('Change line no-5 according to Module 5'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
